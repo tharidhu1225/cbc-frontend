6f112d6c29ed29d576e621ddff56ba91331746aa
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-01-29 23:32:41 +0530

    use tailwindCss

---
+++ pages/logingPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage(){
+  return(
+    <div>
+        <button className="bg-blue-500 p-">Login</button>
+    </div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginPage from '../pages/logingPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+   <LoginPage/>
+
+    </>
+  )
+}
+
+export default App
